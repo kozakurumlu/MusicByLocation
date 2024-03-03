c2ec7cb471a1944b77c78c0e8409549c10fe2ec6
Koza Kurumlu <kozakurumlu@gmail.com>
2024-03-03 14:41:15 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Kurumlu, Koza (RCH) on 03/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
